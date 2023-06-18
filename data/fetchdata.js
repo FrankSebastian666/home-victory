5d2fb1103f44a49fc9e60dbd955eee794ed85bee
sfrank <sebastian_frank@posteo.de>
2023-06-18 19:41:58 +0200

    breaking: new boilerplate

--- data/fetchdata.js
+++
@@ -1,6 +0,0 @@
-export async function getData() {
-    // Instead of the file system,
-    // fetch post data from an external API endpoint
-    const res = await fetch("..");
-    return res.json();
-}
