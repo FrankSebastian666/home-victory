5dacb87ba3e5c8d698978e127e9283fb4dc8ee92
sfrank <sebastian_frank@posteo.de>
2023-06-30 13:11:05 +0200

    chore: wip

--- app/basic-charts/page.tsx
+++ app/basic-charts/page.tsx
@@ -1,4 +1,34 @@
+"use client";
+
+import { VictoryBar, VictoryChart } from "victory";
+
+// here goes the data for the charts
+const firstVictoryBarData = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+];
+
 // this is the UI for the basic-charts URL
 export default function Page() {
-    return <h1>Basic Charts with Victory</h1>;
+    return (
+        <main>
+            <div>
+                <h1>Basic Charts with Victory</h1>
+            </div>
+
+            <div className="basic-victory-bar">
+                <VictoryChart>
+                    <VictoryBar
+                        data={firstVictoryBarData}
+                        // data accessor for x values
+                        x="quarter"
+                        // data accessor for y values
+                        y="earnings"
+                    />
+                </VictoryChart>
+            </div>
+        </main>
+    );
 }
